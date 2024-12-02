3c35c39ac29fb10ef58fcef0d2b694a5db81abd4
Raul Tierno <raul.tierno@juntaex.es>
2024-12-02 09:14:50 +0100

    style: :art: preparo index

---
+++ src/js/tips.js
@@ -0,0 +1,21 @@
+export function getTips() {
+    return [
+        {
+            category: 'Reto #02',
+            tips: [
+                'Enmarcando Nombres',
+                'Fácil',
+                'Se presta a refactorizacion para dejarlo más limpio'
+            ]
+        },
+        {
+            category: 'Reto #01 ',
+            tips: [
+                'Primer Regalo Repetido',
+                'Fácil',
+                'Entretenido'
+            ]
+        },
+        
+    ];
+}
