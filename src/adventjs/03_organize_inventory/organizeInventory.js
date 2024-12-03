f08518ce97754f79d5d5ebb1b390384c4bd23b67
Raul Tierno <raul.tierno@juntaex.es>
2024-12-03 07:59:32 +0100

    perf(challenge_03): :zap: parece que la desestructuracion afecta positivamente al rendimiento 5 estrellas

--- src/adventjs/03_organize_inventory/organizeInventory.js
+++ src/adventjs/03_organize_inventory/organizeInventory.js
@@ -1,7 +1,10 @@
 export const organizeInventory = (inventory) => {
-    return inventory.reduce((acc, item) => {
-      const category = acc[item.category] ||= {}; // Inicializa la categoría si no existe
-      category[item.name] = (category[item.name] || 0) + item.quantity;
-      return acc;
-    }, {});
+    const organized = {};
+
+  for (const { category, name, quantity } of inventory) {
+    const categoryObj = organized[category] ||= {};
+    categoryObj[name] = (categoryObj[name] || 0) + quantity;
+  }
+
+  return organized;
   }
