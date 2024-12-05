12e758e326395d25525c8f626cbcb42e62647bc1
Raul Tierno <raul.tierno@juntaex.es>
2024-12-05 08:21:10 +0100

    chore(challenge_05): :construction: probamos functional programming sin reduce pero el rendimiento se va a la mierda

--- src/adventjs/05_organize_shoes/organizeShoes.js
+++ src/adventjs/05_organize_shoes/organizeShoes.js
@@ -1,14 +1,20 @@
 export const organizeShoes = (shoes) => {
 
-    return shoes.reduce((acc, { size, type }) => {
-        const oppositeKey = `${size}-${type === 'I' ? 'R' : 'I'}`
-        if (acc.counts[oppositeKey]) {
-            acc.counts[oppositeKey]--
-            acc.pairs.push(size)
-        } else {
-            const currentKey = `${size}-${type}`
-            acc.counts[currentKey] = (acc.counts[currentKey] || 0) + 1
-        }
-        return acc;
-    }, { counts: {}, pairs: [] }).pairs
+    const counts = shoes
+        .map(({ size, type }) => `${size}-${type}`)
+        .reduce((acc, key) => {
+            acc[key] = (acc[key] || 0) + 1
+            return acc
+        }, {})
+
+    const pairs = Object.entries(counts)
+        .filter(([key]) => key.includes('-I'))
+        .flatMap(([key, countI]) => {
+            const size = key.split('-')[0]
+            const oppositeKey = `${size}-R`
+            const countR = counts[oppositeKey] || 0
+            return Array(Math.min(countI, countR)).fill(Number(size))
+        })
+
+    return pairs
 }
