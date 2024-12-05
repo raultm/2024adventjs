8b66d2dc1508d911f8c962ff735d1350c3bfb3a4
Raul Tierno <raul.tierno@juntaex.es>
2024-12-05 07:59:25 +0100

    perf(challenge_05): :zap: mejoramos performance con desestructuracion y uso de flatMap

--- src/adventjs/05_organize_shoes/organizeShoes.js
+++ src/adventjs/05_organize_shoes/organizeShoes.js
@@ -1,16 +1,12 @@
 export const organizeShoes = (shoes) => {
     
-    const shoesBySize = shoes.reduce((acc, shoe) => {
-        const shoeType = shoe.type;
-        const shoeSize = shoe.size;
-        acc[shoeSize] = acc[shoeSize] || {size: shoeSize, "I":0, "R":0};
-        acc[shoeSize][shoeType]+= 1;
+    const shoesBySize = shoes.reduce((acc, { size, type }) => {
+        if (!acc[size]) acc[size] = { I: 0, R: 0 };
+        acc[size][type] += 1;
         return acc;
     }, {});
 
-    let pairs = [];
-    Object.keys(shoesBySize).forEach(size => {
-        pairs.push(...Array(Math.min(shoesBySize[size].I, shoesBySize[size].R)).fill(Number(size)));
-    })
-    return pairs
+    return Object.entries(shoesBySize).flatMap(([size, { I, R }]) =>
+        Array(Math.min(I, R)).fill(Number(size))
+    )
 }
