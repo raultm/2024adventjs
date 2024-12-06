eb7816c9526abec8c12a7b3707948e2a6bc9c4e2
raultm <raultm@gmail.com>
2024-12-06 18:35:28 +0100

    refactor(challenge_06): :recycle: solucion con programacion funcional

--- src/adventjs/06_inside_the_box/inBox.js
+++ src/adventjs/06_inside_the_box/inBox.js
@@ -1,15 +1,8 @@
 export const inBox = (box) => {
-    for (let i = 1; i < box.length - 1; i++) {
-        const row = box[i];
-
-        if (row.includes('*')) {
+    return box
+        .slice(1, -1) 
+        .some(row => {
             const starIndex = row.indexOf('*');
-
-            if (starIndex > 0 && starIndex < row.length - 1) {
-                return true
-            }
-        }
-    }
-
-    return false
+            return starIndex > 0 && starIndex < row.length - 1;
+        });
 }
